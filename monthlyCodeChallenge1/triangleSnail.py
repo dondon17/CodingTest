ef7a2b6d80b6a787b0bafbe8ec3fdd0e6f15564d
dondon <qwerqw889@ajou.ac.kr>
2021-05-15 11:36:54 +0900

    월코챌 삼각달팽이 업데이트

--- monthlyCodeChallenge1/triangleSnail.py
+++ monthlyCodeChallenge1/triangleSnail.py
@@ -11,6 +11,7 @@ def solution(n):
     
     for i in range(n):
         for j in range(i, n):
+            # 방향은 3가지(down, right, up) 이므로 mod 3 연산 수행
             if i % 3 == 0:
                 r += 1
             elif i % 3 == 1:
@@ -20,37 +21,44 @@ def solution(n):
                 c -= 1
             temp[r][c] = cnt
             cnt += 1
-    # while cnt <= n*(n+1)//2:
-        
-    #     if direction == 1:
-    #         nr = r + 1
-    #         if nr >= 0 and nr < n and temp[nr][c] == 0:
-    #             r = nr
-    #             temp[r][c] = cnt
-    #         else:
-    #             cnt -= 1 
-    #             direction = 2
 
-    #     elif direction == 2:
-    #         nc = c + 1
-    #         if nc >= 0 and nc < n and temp[r][nc] == 0:
-    #             c = nc
-    #             temp[r][c] = cnt
-    #         else:
-    #             cnt -= 1 
-    #             direction = 3
-        
-    #     elif direction == 3:
-    #         nc = c - 1
-    #         nr = r - 1
-    #         if nc < n and nr < n and nc >= 0 and nr >=0 and temp[nc][nr] == 0:
-    #             r, c = nr, nc
-    #             temp[r][c] = cnt
-    #         else:
-    #             cnt -=1 
-    #             direction = 1
 
-    #     cnt += 1
+    '''    
+    아래 코드로 작성 시, n=7일 때까지 정상 동작, 8이후로 시간 초과
+    쓸데 없는 비교연산이 많이 수행됨
+    
+    while cnt <= n*(n+1)//2:
+        
+        if direction == 1:
+            nr = r + 1
+            if nr >= 0 and nr < n and temp[nr][c] == 0:
+                r = nr
+                temp[r][c] = cnt
+            else:
+                cnt -= 1 
+                direction = 2
+
+        elif direction == 2:
+            nc = c + 1
+            if nc >= 0 and nc < n and temp[r][nc] == 0:
+                c = nc
+                temp[r][c] = cnt
+            else:
+                cnt -= 1 
+                direction = 3
+        
+        elif direction == 3:
+            nc = c - 1
+            nr = r - 1
+            if nc < n and nr < n and nc >= 0 and nr >=0 and temp[nc][nr] == 0:
+                r, c = nr, nc
+                temp[r][c] = cnt
+            else:
+                cnt -=1 
+                direction = 1
+
+        cnt += 1
+    '''
 
     for row in temp:
         print(row)
